352423fd556cd4a87f0767c182eaaf8bf01a2398
kindtiger <seonbeom2@gmail.com>
2024-07-02 13:50:50 +0900

    learn. pocketbase
    - SDK

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,13 +4,33 @@
 
 
 import '@/pages/login/login.css';
-import { setDocumentTitle } from 'kind-tiger'
+import { setDocumentTitle,getNode } from 'kind-tiger'
+
+
+
 
 
 
 
 setDocumentTitle('29CM / 로그인');
 
+
+const loginButton = getNode('.login');
+
+
+function handleLogin(e){
+  e.preventDefault();
+  console.log('clicked!');
+  
+
+}
+
+
+
+
+
+
+loginButton.addEventListener('click',handleLogin);
 
 
 
--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -2,10 +2,12 @@
 
 
 
-import "@/pages/product/product.css"
-import { tiger,insertLast, comma, setDocumentTitle } from "kind-tiger";
-import getPbImageURL from '@/api/getPbImageURL'
-import gsap from 'gsap'
+import "@/pages/product/product.css";
+import { comma, insertLast, setDocumentTitle } from "kind-tiger";
+import getPbImageURL from '@/api/getPbImageURL';
+import gsap from 'gsap';
+import pb from '@/api/pocketbase';
+
 
 
 
@@ -14,12 +16,18 @@ setDocumentTitle('29CM / 상품목록');
 
 async function renderProductItem(){
 
-  const response = await tiger.get(`${import.meta.env.VITE_PB_API}/collections/products/records`)
+  const productData = await pb
+  .collection('products')
+  .getFullList({
+    sort: '-created'
+  }); // SDK
 
-  const productsData = response.data.items;
+
+  // const response = await tiger.get(`${import.meta.env.VITE_PB_API}/collections/products/records`) // Fetch API
+  // const productsData = response.data.items;
 
 
-  productsData.forEach((item)=>{
+  productData.forEach((item)=>{
 
     const discount = item.price * (item.ratio * 0.01);
 
